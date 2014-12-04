a26764a05facfb703b082332e24d661ade1da2c2
Pete Petrash <pete.petrash@gmail.com>
2014-12-03 18:54:50 -0800

    very important updates

--- VideoBackgroundTest/ViewController.swift
+++ VideoBackgroundTest/ViewController.swift
@@ -8,8 +8,10 @@ class VideoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // dark theme
         view.backgroundColor = UIColor.blackColor()
         
+        // add video from instagram
         var url:NSURL? = NSURL(string: "http://videos-a-5.ak.instagram.com/hphotos-ak-xpa1/10461296_834306619932165_1563854104_n.mp4")
         moviePlayer = MPMoviePlayerController(contentURL: url)
         moviePlayer.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
@@ -21,13 +23,18 @@ class VideoViewController: UIViewController {
         moviePlayer.shouldAutoplay = true
         moviePlayer.view.alpha = 0.8
         
-        var label = UILabel(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
-        label.center = CGPointMake(160, 284)
+        
+        // add label
+        var screenWidth = self.view.frame.width
+        var screenHeight = self.view.frame.height
+        var label = UILabel(frame: CGRectMake(0, 0, screenWidth, screenHeight))
+        label.center = CGPointMake(screenWidth/2, screenHeight/2)
         label.textColor = UIColor.whiteColor()
-        label.textAlignment = NSTextAlignment.Center
-        label.text = "awwwww yeah"
+        
+        label.text = "⚡️\n awwwww\n yeah\n ⚡️"
         label.font = UIFont(name: label.font.fontName, size: 70)
-        label.numberOfLines = 2
+        label.textAlignment = NSTextAlignment.Center
+        label.numberOfLines = 4
         self.view.addSubview(label)
     }
     
